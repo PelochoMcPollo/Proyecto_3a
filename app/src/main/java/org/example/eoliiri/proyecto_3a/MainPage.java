41cbc09125a9c3e891d468bb97b7d8056d5ef507
nordin001 <73000909+nordin001@users.noreply.github.com>
2023-12-15 01:15:33 +0100

    c

--- app/src/main/java/org/example/eoliiri/proyecto_3a/MainPage.java
+++ app/src/main/java/org/example/eoliiri/proyecto_3a/MainPage.java
@@ -1,13 +1,27 @@
 package org.example.eoliiri.proyecto_3a;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPage extends AppCompatActivity {
+    TextView co2,temperatura,riesgo;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+        co2=findViewById(R.id.CO2);
+        temperatura=findViewById(R.id.Temp);
+        riesgo=findViewById(R.id.nivelRiesgo);
+
+
+
+
+        /*co2.setText(co2p);
+        temperatura.setText(tempp);
+        riesgo.setText();*/
     }
 }
